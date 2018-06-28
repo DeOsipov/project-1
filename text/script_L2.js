5ebf00fbeea809db8670d61f5b405b366bf65af3
DeOsipov <39943741+DeOsipov@users.noreply.github.com>
2018-06-28 03:22:11 +0300

    delete space in index_L2

--- text/script_L2.js
+++ text/script_L2.js
@@ -1,17 +1,17 @@
-var svgImgMinus = '<svg width="20"height="20"><circle r="10"cx="10"cy="10"fill="white" /><line x1="4"y1="10"x2="16"y2="10"stroke="black"stroke-width="2" /></svg><span>'
-var svgImgPlus = '<svg width="20"height="20"><circle r="10"cx="10"cy="10"fill="white" /><line x1="4"y1="10"x2="16"y2="10"stroke="black"stroke-width="2" /><line x1="10"y1="4"x2="10"y2="16"stroke="black"stroke-width="2" /></svg><span>'
+var svgImgMinus = '<svg width="20"height="20"><circle r="10"cx="10"cy="10"fill="white" /><line x1="4"y1="10"x2="16"y2="10"stroke="black"stroke-width="2" /></svg>'
+var svgImgPlus = '<svg width="20"height="20"><circle r="10"cx="10"cy="10"fill="white" /><line x1="4"y1="10"x2="16"y2="10"stroke="black"stroke-width="2" /><line x1="10"y1="4"x2="10"y2="16"stroke="black"stroke-width="2" /></svg>'
 
 function addOnClick(button, informer, text){
     let className = informer.className;
     if( className.indexOf(' expanded') == -1 ){
         className += ' expanded';
-        button.innerHTML = svgImgMinus + text + '</span>';
+        button.innerHTML = svgImgMinus + text;
     	button.style.fontStyle = 'normal';
         button.style.textTransform = "uppercase";
         button.style.fontSize = "25px";
     } else {
         className = className.replace(' expanded', '');
-        button.innerHTML = svgImgPlus + text + '</span>';
+        button.innerHTML = svgImgPlus + text;
         button.style.fontStyle = 'italic';
         button.style.textTransform = "lowercase";
         button.style.fontSize = "25px";
